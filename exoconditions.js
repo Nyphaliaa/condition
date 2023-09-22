f9d3e96d1397bbc874f7b529ee39f348526cfc79
Nyphaliaa <145330530+Nyphaliaa@users.noreply.github.com>
2023-09-22 11:40:28 +0200

    Add files via upload

---
+++ exoconditions.js
@@ -0,0 +1,23 @@
+"use strict";
+
+/* créer 3 variables
+
+a égal à 4
+b égal à 4
+c égal à 3
+
+*/
+let a=4;
+let b=4;
+let c=3;
+
+switch(a){ // switch pour tester des égalités
+    case b:
+        console.log("égal à b"); // si a est égal à b
+        break;
+    case c:
+        console.log("égal à c"); // si a est égal à c
+        break;
+    default:
+        console.log("égal à rien"); // si a est égal à rien
+}
